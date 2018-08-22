97004b00dcd7f2536cbdf67a38e52367c2aa92ce
pleamon <pleamon.li@gmail.com>
2018-08-22 20:04:48 +0800

    update handle name

--- server_handle.go
+++ server_handle.go
@@ -2,15 +2,15 @@ package pnet
 
 type ServerHandler interface {
 	// 服务端程序启动成功调用
-	Initinize(*Server)
+	OnServerInitinize(*Server)
 	// 有新客户端连接时调用
-	AcceptConnHandle(*Server, *ClientInfo) ([]byte, error)
+	OnAccept(*Server, *ClientInfo) ([]byte, error)
 	// 心跳检测
-	HeathHandle(*Server) error
+	OnHeath(*Server) error
 	// 接收到客户端数据时调用
-	ReceiveMessageHandle(*Server, *Message) ([]byte, error)
+	OnReceive(*Server, *Message) ([]byte, error)
 	// 客户端连接断开时调用
-	FinishConnHandle(error)
+	OnClose(error)
 	// 程序错误时调用
-	ErrorHandle(int, interface{})
+	OnError(int, interface{})
 }
