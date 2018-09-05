1ce6b12b886ad95a31c1ea2b1472bf1189f61a1a
pleamon <pleamon.li@gmail.com>
2018-09-05 18:17:42 +0800

    更换checksum算法

--- readwriter.go
+++ readwriter.go
@@ -6,17 +6,16 @@ import (
 	"context"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
-	"math"
+	"log"
 	"net"
 
 	"git.pleamon.com/p/plog"
 )
 
 const (
-	BLOCKSIZE = 4096
+	CHECKSUMFLAG = 1024
 )
 
 var (
@@ -25,15 +24,16 @@ var (
 
 type ReadWriter struct {
 	ClientId  string
-	MessageID uint64
+	MessageID uint32
 	*bufio.Writer
 	*bufio.Reader
-	LenPlace int
+	LenPlace      int
+	CheckSumPlace int
 }
 
 type Message struct {
 	ClientID string
-	Length   int64
+	Length   uint32
 	Data     []byte
 	err      error
 }
@@ -42,11 +42,12 @@ func NewReaderWriterFromConn(clientId string, conn net.Conn) *ReadWriter {
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 	rw := &ReadWriter{
-		ClientId:  clientId,
-		Reader:    reader,
-		Writer:    writer,
-		LenPlace:  8,
-		MessageID: 0,
+		ClientId:      clientId,
+		Reader:        reader,
+		Writer:        writer,
+		LenPlace:      4,
+		CheckSumPlace: 4,
+		MessageID:     0,
 	}
 	return rw
 }
@@ -57,29 +58,38 @@ func (rw *ReadWriter) ResetMessageId() {
 
 func (rw *ReadWriter) ReadPack() (*Message, error) {
 	dataLength, err := rw.ReadPackLen()
-	msg := &Message{
-		ClientID: rw.ClientId,
-	}
 	if err != nil {
-		plog.Debug(err)
+		plog.Error(err)
 		return nil, err
 	}
-	if dataLength%BLOCKSIZE != 0 {
-		// 丢弃数据
+	checkSum, err := rw.ReadCheckSum()
+	if err != nil {
+		plog.Error(err)
+	}
+	if checkSum == 0 {
+		plog.DebugF("checksum is %d, discard conn buffer data", checkSum)
 		io.Copy(ioutil.Discard, rw)
-		return nil, errors.New("check sum failed")
+		return nil, nil
+	}
+	if (dataLength+checkSum)%CHECKSUMFLAG != 0 {
+		plog.DebugF("(checksum[%d] + datalength[%d]) % 1024 == 0, discard conn buffer data", checkSum, dataLength)
+		io.Copy(ioutil.Discard, rw)
+		return nil, nil
 	}
 	data, err := rw.ReadPackData(dataLength)
 	if err != nil {
-		plog.Debug(err)
+		plog.Error(err)
 		return nil, ErrorCheckSumFailed
 	}
-	msg.Length = dataLength
-	msg.Data = data
+	msg := &Message{
+		ClientID: rw.ClientId,
+		Length:   dataLength,
+		Data:     data,
+	}
 	return msg, nil
 }
 
-func (rw *ReadWriter) ReadPackLen() (int64, error) {
+func (rw *ReadWriter) ReadPackLen() (uint32, error) {
 	dataSizeByte := make([]byte, rw.LenPlace)
 	_, err := rw.Read(dataSizeByte)
 	if err != nil {
@@ -87,62 +97,79 @@ func (rw *ReadWriter) ReadPackLen() (int64, error) {
 		return 0, err
 	}
 	dataSizeBuffer := bytes.NewBuffer(dataSizeByte)
-	var dataLength int64
+	var dataLength uint32
 	binary.Read(dataSizeBuffer, binary.BigEndian, &dataLength)
+	log.Println("datalength", dataLength)
 
 	return dataLength, nil
 }
 
-func (rw *ReadWriter) ReadPackData(length int64) ([]byte, error) {
-	if length > math.MaxInt64 {
-		plog.DebugF("read pack data out of range [%d], current system max length [%d]", length, math.MaxInt64)
-		return nil, fmt.Errorf("read pack data out of range [%d], current system max length [%d]", length, math.MaxInt64)
+func (rw *ReadWriter) ReadCheckSum() (uint32, error) {
+	dataSizeByte := make([]byte, rw.CheckSumPlace)
+	_, err := rw.Read(dataSizeByte)
+	if err != nil {
+		plog.Debug(err)
+		return 0, err
 	}
-	buffer := &bytes.Buffer{}
-	for {
-		if length <= 0 {
-			break
-		}
-		dataByte := make([]byte, BLOCKSIZE)
-		n, err := rw.Read(dataByte)
-		if err != nil {
-			plog.Error(err)
-			return nil, err
-		}
-		buffer.Write(dataByte[0:n])
-		length = length - BLOCKSIZE
+	dataSizeBuffer := bytes.NewBuffer(dataSizeByte)
+	var dataCheckSum uint32
+	binary.Read(dataSizeBuffer, binary.BigEndian, &dataCheckSum)
+	log.Println("dataCheckSum", dataCheckSum)
+
+	return dataCheckSum, nil
+}
+
+func (rw *ReadWriter) ReadPackData(length uint32) ([]byte, error) {
+	dataByte := make([]byte, length)
+	_, err := rw.Read(dataByte)
+	if err != nil {
+		plog.Error(err)
+		return nil, err
 	}
-	return buffer.Bytes(), nil
+	return dataByte, nil
 }
 
 func (rw *ReadWriter) WritePack(dataByte []byte) error {
-	if len(dataByte) == 0 {
+	length := len(dataByte)
+	if length == 0 {
 		return errors.New("not data")
 	}
-	length := len(dataByte)
-	_length := length + (BLOCKSIZE - length%BLOCKSIZE)
-	dataLength := uint64(_length)
+	dataLength := uint32(length)
+	mold := length % CHECKSUMFLAG
+	checkSum := uint32(CHECKSUMFLAG - mold)
+	if checkSum == 0 {
+		checkSum = CHECKSUMFLAG
+	}
 	encodeData := dataByte
 	respPackLen := make([]byte, rw.LenPlace)
-	binary.BigEndian.PutUint64(respPackLen, dataLength)
+	binary.BigEndian.PutUint32(respPackLen, dataLength)
+
+	respCheckSum := make([]byte, rw.CheckSumPlace)
+	binary.BigEndian.PutUint32(respCheckSum, checkSum)
 
 	buffer := &bytes.Buffer{}
 
 	_, err := buffer.Write(respPackLen)
 	if err != nil {
-		plog.Debug(err)
+		plog.Error(err)
+		return err
+	}
+
+	_, err = buffer.Write(respCheckSum)
+	if err != nil {
+		plog.Error(err)
 		return err
 	}
 
 	_, err = buffer.Write(encodeData)
 	if err != nil {
-		plog.Debug(err)
+		plog.Error(err)
 		return err
 	}
 
 	_, err = rw.Write(buffer.Bytes())
 	if err != nil {
-		plog.Debug(err)
+		plog.Error(err)
 		return err
 	}
 	return rw.Flush()
